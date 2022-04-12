a817e923e627c36ba59e98b40ebad3b182483ea7
Markus <72389013+DWMarkus@users.noreply.github.com>
2022-04-12 11:44:53 +0200

    update hub

---
+++ functions.js
@@ -0,0 +1,9 @@
+function Allumer() {
+    document.getElementById('ampoff').style.display='none', 
+    document.getElementById('ampon').style.display='block';
+  }
+
+function Eteindre() {
+  document.getElementById('ampon').style.display='none', 
+  document.getElementById('ampoff').style.display='block';
+}
--- sampleServer.js
+++ sampleServer.js
@@ -1,12 +1,61 @@
-var http = require('http'); 
+let http = require('http'); 
 var port = 8080;
 var url = require('url');
+let fs = require('fs');
 
-var server = http.createServer(function(req, res) { 
-    res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('Bien le bonjour !<p>Voici un paragraphe <stong>HTML</strong></p>')
-    res.end();
-}); 
+// express
 
-server.listen(port);
+let handleRequest = (request, res) => {
+    var page = url.parse(request.url).pathname;
+    res.writeHead(200, {
+        "Content-Type": 'text/html'
+    });
+
+    if(page == '/') {
+        res.write('Salut tout le monde ! <br>');
+        res.write('<a href="/ampoule">Ampoule</a> <br>');
+        res.write('<a href="/captcha">Captcha</a> <br>');
+        res.write('<a href="/hash">Hash</a> <br>');
+        res.end();
+    }
+
+    if(page == '/ampoule') {
+        fs.readFile('./OnOff.html', null, function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Whoops! Fichier inconnu.');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        }); 
+    };
+
+    if(page == '/captcha') {
+        fs.readFile('./captcha.html', null, function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Whoops! Fichier inconnu.');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        }); 
+    };
+
+    if(page == '/hash') {
+        fs.readFile('./hash.html', null, function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Whoops! Fichier inconnu.');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        }); 
+    };
+ 
+};
+
+http.createServer(handleRequest).listen(port);
 console.log('Le serveur répond sur http://127.0.0.1:' + port + '/');
